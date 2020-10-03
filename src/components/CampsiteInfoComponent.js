f70f88e283e12fe4a0d03874c77b58adaf6b24e8
Salih Sarikaya <salihsarikaya34@gmail.com>
2020-10-03 09:13:18 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ return (
 )
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
 if (comments) {
     return (
         <div className="col-md-5 m-1">
@@ -39,7 +39,7 @@ if (comments) {
                     );
                 })
             }
-            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+            <CommentForm campsiteId={campsiteId} postComment={postComment} />
         </div>
     );
 }
@@ -68,7 +68,7 @@ toggleModal() {
 
 handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
 }
 
 render() {
@@ -178,11 +178,11 @@ function CampsiteInfo(props) {
             </div>
             <div className="row">
                 <RenderCampsite campsite={props.campsite} />
-                <RenderComments 
-                        comments={props.comments}
-                        addComment={props.addComment}
-                        campsiteId={props.campsite.id}
-                    />
+                <RenderComments
+                    comments={props.comments}
+                    postComment={props.postComment}
+                    campsiteId={props.campsite.id}
+                />
             </div>
         </div>
     );
